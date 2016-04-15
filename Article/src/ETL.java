fb6a011e444c0431f244ee26b1b5517843fe001c
helunwencser <lunwenh@andrew.cmu.edu>
2016-04-14 22:24:57 -0700

    update

--- Article/src/ETL.java
+++ Article/src/ETL.java
@@ -1,13 +1,10 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
